317f8929d554df46261d886803b58be6bd5a08b3
Vivekananda Reddy <vivekanandapolu123@gmail.com>
2022-02-10 21:56:43 +0530

    Added the new end points and added some controllers in user userroutes & controllers

--- src/Users/user.controller.js
+++ src/Users/user.controller.js
@@ -1,5 +1,5 @@
 import httpStatusCode from "http-status-codes";
-import { isValidEmail, encryptPassword } from "../../uitility.js";
+import { isValidEmail, encryptPassword, comparePassword } from "../../uitility.js";
 import UserModel from "./user.model.js";
 export default class UserController {
   async userSignup(request, response) {
@@ -29,7 +29,7 @@ export default class UserController {
     }
   }
   //To find all queries in the database
-  async findall(request, response) {
+  async allUsers(request, response) {
     try {
       const user = await UserModel.find();
       if (user) {
@@ -40,7 +40,7 @@ export default class UserController {
     }
   }
   //Finding the data by using the default:_Id in database
-  async findById(request, response) {
+  async user(request, response) {
     try {
       const user = await UserModel.findById(request.params._id)
       console.log(user);
@@ -49,6 +49,37 @@ export default class UserController {
       }
     } catch (error) {
       response.status(httpStatusCode.OK).send(error)
+    }
+  }
+  async userLogin(request, response) {
+    try {
+      //Step:1 finding the existing User by using user
+      let user = await UserModel.findOne({
+        username: request.body.username
+      });
+      if (!user)
+        return response
+          .status(httpStatusCode.INTERNAL_SERVER_ERROR)
+          .send({ message: "Enter a valid Username" });
+
+      //Compare password
+      let passwordStatus = await comparePassword(
+        request.body.password,
+        user.password
+      );
+      if (passwordStatus) {
+        delete user.password;
+        return response
+          .status(httpStatusCode.OK)
+          .send({ message: "User Logged in sucessfully", data: user });
+      }
+      return response
+        .status(httpStatusCode.INTERNAL_SERVER_ERROR)
+        .send({ message: "Please enter a valid password" });
+    } catch (error) {
+      response
+        .status(httpStatusCode.INTERNAL_SERVER_ERROR)
+        .send({ message: error.message });
     }
   }
 }
--- src/Users/user.routes.js
+++ src/Users/user.routes.js
@@ -2,7 +2,8 @@ import UserController from "./user.controller.js";
 const userData = new UserController();
 async function userRotes(router) {
   router.post("/api/user/singup", userData.userSignup);
-  router.get("/api/users/all", userData.findall)
-  router.get("/api/user/:_id", userData.findById)
+  router.get("/api/users/all", userData.allUsers)
+  router.get("/api/user/:_id", userData.user)
+  router.post("/api/user/login", userData.userLogin)
 }
 export default userRotes;
